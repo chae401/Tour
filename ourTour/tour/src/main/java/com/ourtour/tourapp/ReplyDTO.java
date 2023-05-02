a5a04b8c74b4a48f2ca180245ee7cafb9b4a15d5
MinDongbeom <127920478+MinDongbeom@users.noreply.github.com>
2023-05-02 13:11:08 +0900

    Add files via upload

---
+++ ourTour/tour/src/main/java/com/ourtour/tourapp/ReplyDTO.java
@@ -0,0 +1,8 @@
+package com.ourtour.board;
+public class ReplyDTO {
+	private String replyNo;
+	private int boardNo;
+	private String writedate;
+	private String replycontent;
+	private String userID;
+}
