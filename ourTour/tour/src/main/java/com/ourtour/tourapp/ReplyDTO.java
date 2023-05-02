a24e7f9a31725f0b26b3d57232d2fc2ca6dd36b4
MinDongbeom <127920478+MinDongbeom@users.noreply.github.com>
2023-05-02 13:17:39 +0900

    Delete ReplyDTO.java

--- ourTour/tour/src/main/java/com/ourtour/tourapp/ReplyDTO.java
+++
@@ -1,8 +0,0 @@
-package com.ourtour.board;
-public class ReplyDTO {
-	private String replyNo;
-	private int boardNo;
-	private String writedate;
-	private String replycontent;
-	private String userID;
-}
